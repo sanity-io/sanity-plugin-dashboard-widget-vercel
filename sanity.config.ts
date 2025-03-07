ee5e261d552f2837cd3bc2080675fa779fafce38
Cody Olsen <stipsan@gmail.com>
2025-03-07 11:52:34 +0100

    chore: add vision tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,9 @@
 import {defineConfig} from 'sanity'
+import {visionTool} from '@sanity/vision'
 
 export default defineConfig({
   name: 'sanity-plugin-dashboard-widget-vercel',
   projectId: 'ppsg7ml5',
   dataset: 'test',
+  plugins: [visionTool()],
 })

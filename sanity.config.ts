80b45fe4671a8d695cce5eb52b2780731fc549d9
Cody Olsen <stipsan@gmail.com>
2025-03-07 11:55:09 +0100

    chore: setup dashboard widget

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,19 @@
 import {defineConfig} from 'sanity'
+import {dashboardTool} from '@sanity/dashboard'
 import {visionTool} from '@sanity/vision'
+import {vercelWidget} from './src'
 
 export default defineConfig({
   name: 'sanity-plugin-dashboard-widget-vercel',
   projectId: 'ppsg7ml5',
   dataset: 'test',
-  plugins: [visionTool()],
+  plugins: [
+    dashboardTool({
+      widgets: [vercelWidget()],
+    }),
+    visionTool(),
+  ],
+  scheduledPublishing: {
+    enabled: false,
+  },
 })
